1d616557f8459a67baf4e0cea2f6091175f95355
Matti Luukkainen <mluukkai@iki.fi>
2018-01-28 17:59:31 +0200

    Update reducer.js

--- src/reducer.js
+++ src/reducer.js
@@ -19,8 +19,11 @@ const asObject = (anecdote) => {
 
 const initialState = anecdotesAtStart.map(asObject)
 
-const reducer = (store = initialState, action) => {
-  return store
+const reducer = (state = initialState, action) => {
+  console.log('state now: ',state)
+  console.log('action', action)
+  
+  return state
 }
 
 export default reducer
